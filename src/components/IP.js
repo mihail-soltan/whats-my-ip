764369959109e27ec31fcf9d364a8c5f50a2f922
mihail <mihail.soltan@protonmail.ch>
2021-07-08 14:11:00 +0200

    fix data display

--- src/components/IP.js
+++ src/components/IP.js
@@ -3,33 +3,31 @@ import { useEffect, useState } from "react"
 
 export default function IP() {
     // console.log(process.env)
-    // console.log(data.location.country)
+    console.log(data.location.country)
     const [userData, setUserData] = useState([])
     
     
     useEffect(() => {
-        setUserData(data)
-        console.log(userData)
+        setUserData([data])
         
+        console.log(userData)
     }, [])
     return (
        
             <>
-            { [userData].map((item)=>
+            { userData.map( (item)=>
             <ul>
              <li> 
-              {item.ip} 
-              </li>
-              <li> 
-              {item.location.country} 
-              </li>
-              <li> 
-              {item.location.lat} 
+              {item.ip}         
+
               </li>
               <li>
-                  
-              {item.location.lng}
+                  {item.location.country}
               </li>
+              <li>
+                  {item.location.lat}, {item.location.lng}
+              </li>
+
               </ul>)} 
           </>  
         
