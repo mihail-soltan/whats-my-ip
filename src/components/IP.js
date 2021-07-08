85313d58aa932d29ef94750834115b417a27e7b1
mihail <mihail.soltan@protonmail.ch>
2021-07-08 12:45:50 +0200

    initial commit

---
+++ src/components/IP.js
@@ -0,0 +1,37 @@
+import data from "./data" 
+import { useEffect, useState } from "react"
+
+export default function IP() {
+    // console.log(process.env)
+    // console.log(data.location.country)
+    const [userData, setUserData] = useState([])
+    
+    
+    useEffect(() => {
+        setUserData(data)
+        console.log(userData)
+        
+    }, [])
+    return (
+       
+            <>
+            { [userData].map((item)=>
+            <ul>
+             <li> 
+              {item.ip} 
+              </li>
+              <li> 
+              {item.location.country} 
+              </li>
+              <li> 
+              {item.location.lat} 
+              </li>
+              <li>
+                  
+              {item.location.lng}
+              </li>
+              </ul>)} 
+          </>  
+        
+    )
+}
---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+const data={
+
+    "ip": "87.148.154.90",
+    "location": {
+        "country": "DE",
+        "region": "Lower Saxony",
+        "city": "Göttingen",
+        "lat": 51.53443,
+        "lng": 9.93228,
+        "postalCode": "",
+        "timezone": "+02:00",
+        "geonameId": 2918632
+    },
+    "domains": [
+        "p57949a5a.dip0.t-ipconnect.de"
+    ],
+    "as": {
+        "asn": 3320,
+        "name": "Deutsche Telekom",
+        "route": "87.128.0.0/11",
+        "domain": "https://globalcarrier.telekom.com",
+        "type": "NSP"
+    },
+    "isp": "Deutsche Telekom AG",
+    "proxy": {
+        "proxy": false,
+        "vpn": false,
+        "tor": false
+    }
+
+}
+export default data
