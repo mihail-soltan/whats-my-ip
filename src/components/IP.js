7471b66e27a64fd733f58a60674509a380230dd6
KhaledAlabssi <khaled.absi5@gmail.com>
2021-07-10 14:53:34 +0200

    Adding and rendering map component with users data

--- src/components/IP.js
+++ src/components/IP.js
@@ -1,65 +1,69 @@
-import data from "./data" 
+import data from "./data"
 import { useEffect, useState } from "react"
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Map from "./Map"
+
+
 
 export default function IP() {
-    // console.log(process.env)
-    // console.log(data.location.country)
-    const [userData, setUserData] = useState([])
-    const [userData1, setUserData1] = useState([])
-    const apiLink =`https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`
-    const [lat, setLat] = useState(1)
-    const [lng, setLng] = useState(1)
-    
-    useEffect(() => {
-        setUserData([data])
-        
-        // console.log(userData)
-        fetch(apiLink)
-        .then((res) => {
-            if (res.ok) {
-              return res.json()
-            } else {
-              throw new Error("Error!")
-            }
-          })
-          .then((data1) => {
-            setUserData1([data1])
-            setLat(data1.location.lat)
-            setLng(data1.location.lng)
-          })
-          .catch((err) => console.log(err))
-    }, [])
-    return (
-       
-            <>
-            <h1>Data from data.js</h1>
-            { userData.map( (item)=>
-            <ul className = 'data-list'>
-             <li key={Math.random()*1000}> 
-              {item.ip}         
+  // console.log(process.env)
+  // console.log(data.location.country)
+  const [userData, setUserData] = useState([])
+  const [userData1, setUserData1] = useState([])
+  const apiLink = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`
+  const [lat, setLat] = useState(1)
+  const [lng, setLng] = useState(1)
 
-              </li>
-              <li key={Math.random()*1000}>
-                  {item.location.country}
-              </li>
-              <li key={Math.random()*1000}>
-                  {item.location.lat}, {item.location.lng}
-              </li>
+  useEffect(() => {
+    setUserData([data])
 
-              </ul>)} 
-              <h2>Response from API</h2>
-              {
-                  userData1.map((item) =>
-                  <ol>
-                      <li key={Math.random()*1000}>{item.ip}</li>
-                      <li key={Math.random()*1000}>{item.location.lat}, {item.location.lng}</li>
-                  </ol>
+    // console.log(userData)
+    fetch(apiLink)
+      .then((res) => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          throw new Error("Error!")
+        }
+      })
+      .then((data1) => {
+        setUserData1([data1])
+        setLat(data1.location.lat)
+        setLng(data1.location.lng)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+  return (
 
-                  )
-              }
-              
-          </>  
-        
-    )
+    <>
+      <h1>Data from data.js</h1>
+      {userData.map((item) =>
+        <ul className='data-list'>
+          <li key={Math.random() * 1000}>
+            {item.ip}
+
+          </li>
+          <li key={Math.random() * 1000}>
+            {item.location.country}
+          </li>
+          <li key={Math.random() * 1000}>
+            {item.location.lat}, {item.location.lng}
+          </li>
+
+        </ul>)}
+      <h2>Response from API</h2>
+      {
+        userData1.map((item) =>
+          <ol>
+            <li key={Math.random() * 1000}>{item.ip}</li>
+            <li key={Math.random() * 1000}>{item.location.lat}, {item.location.lng}</li>
+          </ol>
+
+        )
+      }
+      <Map lat={lat} lng={lng} />
+      
+
+    </>
+
+  )
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,23 @@
-export default function Map () {
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+export default function Map ({lat, lng}) {
 
     return (
-        <h1>Hi</h1>
+        <div>
+
+
+        <MapContainer center={[lat, lng]} zoom={7} scrollWheelZoom={false} style={{ height: "80vh", width: "100%" }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[lat, lng]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+
+      </div>
     )
 }
