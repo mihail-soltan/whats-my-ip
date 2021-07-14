1bf4df1eb87d5345c1ea2e1ff627f5c16b7c21e2
KhaledAlabssi <khaled.absi5@gmail.com>
2021-07-14 21:43:09 +0200

    add region to user displayed info and flag

--- src/components/IP.js
+++ src/components/IP.js
@@ -14,7 +14,7 @@ export default function IP() {
   const [lng, setLng] = useState(1)
   const [flag, setFlag] = useState('')
   const [capital, setCapital] = useState('')
-  const [ region, setRegion] = useState('')
+  const [region, setRegion] = useState('')
 
   useEffect(() => {
     setUserData([data])
@@ -33,29 +33,29 @@ export default function IP() {
         setLat(data1.location.lat)
         setLng(data1.location.lng)
         fetch(`https://restcountries.eu/rest/v2/alpha?codes=${data1.location.country}`)
-              .then((res) => {
-                if (res.ok) {
-                  return res.json()
-                } else {
-                  throw new Error('Erorr!')
-                }
-              })
-              .then((result) => {
-                setFlag(result[0].flag)
-                setRegion(result[0].region)
-                setCapital(result[0].capital)
-                // setLat(result[0].latlng[0])
-                // setLng(result[0].latlng[1])
-                // console.log(result[0].latlng[1])
-              })
+          .then((res) => {
+            if (res.ok) {
+              return res.json()
+            } else {
+              throw new Error('Erorr!')
+            }
+          })
+          .then((result) => {
+            setFlag(result[0].flag)
+            setRegion(result[0].region)
+            setCapital(result[0].capital)
+            // setLat(result[0].latlng[0])
+            // setLng(result[0].latlng[1])
+            // console.log(result[0].latlng[1])
+          })
 
-      
-      
+
+
       })
       .catch((err) => console.log(err))
   }, [])
 
-  
+
   return (
 
     <>
@@ -81,15 +81,15 @@ export default function IP() {
             <li key={Math.random() * 1000}>{item.ip}</li>
             <li key={Math.random() * 1000}>{item.location.lat}, {item.location.lng}</li>
           </ol>
-          
+
 
         )
       }
       <p>Contenent: {region}</p>
       <p>Capital: {capital}</p>
-      <img src={flag} style={{height : 200}}/>
+      <img src={flag} style={{ height: 200 }} />
       <Map lat={lat} lng={lng} />
-      
+
 
     </>
 
